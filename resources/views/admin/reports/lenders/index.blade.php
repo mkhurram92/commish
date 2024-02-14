e9bf3ebcf9db9c6c4638a4b69f0dc656a7cc9e94
Khurram Shahzad Mirza <53279133+mkhurram92@users.noreply.github.com>
2024-02-14 23:39:01 +1030

    Lender Commission Statement Order By Client Name

--- resources/views/admin/reports/lenders/index.blade.php
+++ resources/views/admin/reports/lenders/index.blade.php
@@ -19,7 +19,7 @@
                             <path d="M12 3L2 12h3v8h6v-6h2v6h6v-8h3L12 3zm5 15h-2v-6H9v6H7v-7.81l5-4.5 5 4.5V18z" />
                             <path d="M7 10.19V18h2v-6h6v6h2v-7.81l-5-4.5z" opacity=".3" />
                         </svg><span class="breadcrumb-icon"> Home</span></a></li>
-                <li class="breadcrumb-item active" aria-current="page"> Reports
+                <li class="breadcrumb-item active" aria-current="page"> Reportss
                 </li>
             </ol>
         </div>
