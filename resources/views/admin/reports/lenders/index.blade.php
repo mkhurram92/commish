84baaafd50fdf7f1ecc8c5af1a61428722615fa0
Mirza <mkhurram92@gmail.com>
2024-02-15 15:47:34 +1030

    Test cPanel Sync

--- resources/views/admin/reports/lenders/index.blade.php
+++ resources/views/admin/reports/lenders/index.blade.php
@@ -19,7 +19,7 @@
                             <path d="M12 3L2 12h3v8h6v-6h2v6h6v-8h3L12 3zm5 15h-2v-6H9v6H7v-7.81l5-4.5 5 4.5V18z" />
                             <path d="M7 10.19V18h2v-6h6v6h2v-7.81l-5-4.5z" opacity=".3" />
                         </svg><span class="breadcrumb-icon"> Home</span></a></li>
-                <li class="breadcrumb-item active" aria-current="page"> Reportss
+                <li class="breadcrumb-item active" aria-current="page"> Reports
                 </li>
             </ol>
         </div>
@@ -30,7 +30,7 @@
         <div class="col-lg-12 col-md-12">
             <div class="main-card card add_form_card mb-3">
                 <div class="card-header">
-                    <h5 class="card-title">Lender Filters</h5>
+                    <h5 class="card-title">Lender Filterssss</h5>
                     <div class="ml-auto">
                         <select class="form-control" name="report_type" id="report_type">
                             <option
